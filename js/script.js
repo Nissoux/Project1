12026c696562c43b960d709296ba1fcf370dca33
Nissoux <70206861+Nissoux@users.noreply.github.com>
2020-09-30 17:09:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-/*const slider = document.querySelector('#selection');
-const content = document.querySelector('.slider')
-const nextBtn = document.querySelector('.btn__carousel__next');
-const prevBtn = document.querySelector('.btn__caousel__prev');
-
-let counter = 0;
-nextBtn.addEventListener('click', () => {
-    content.style.transform = 'translateX(-300px)';
-    counter++;
-    console.log(counter);
-});*/
-
 const menuIcon = document.querySelector('.hamburger-menu');
 const navBar = document.querySelector('.navbar-elements');
 
